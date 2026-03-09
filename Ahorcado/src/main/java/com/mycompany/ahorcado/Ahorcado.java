d38bf493d1f0fcd814c4bc46c897ab75b7076369
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2026-03-09 21:17:11 +0100

    Trying the hanging man

--- Ahorcado/src/main/java/com/mycompany/ahorcado/Ahorcado.java
+++ Ahorcado/src/main/java/com/mycompany/ahorcado/Ahorcado.java
@@ -4,13 +4,47 @@
 
 package com.mycompany.ahorcado;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author pablo
  */
 public class Ahorcado {
+    
+    public static String[] incognito(String word){
+        String palabra[] = new String[word.length()]; 
+        for(int i = 0; i <= word.length(); i++){
+            palabra[i] = String.valueOf(word.charAt(i)); 
+        }
+        return palabra; 
+    }
+    
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in); 
+        
+        Random r = new Random(); 
+        
+        String palabras[] = {"primera", "segundo", "tercer"}; 
+        String guess = ""; 
+        boolean guessed = false; 
+        
+        
+        do{
+            String word_selected = palabras[r.nextInt()+ 2]; 
+            String array_word[] = incognito(word_selected); 
+            System.out.println("Take a guess on this word!");
+            
+            for(int i = 0; i < word_selected.length(); i ++){
+                
+            }
+            
+            
+            
+        }while(!guessed); 
+        
+        
     }
 }

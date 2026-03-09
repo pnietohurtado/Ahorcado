50de4f0bbe13f7d4b4e5248e45ee590983a4ae4e
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2026-03-09 20:57:33 +0100

    First commit

---
+++ Ahorcado/src/main/java/com/mycompany/ahorcado/Ahorcado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ahorcado;
+
+/**
+ *
+ * @author pablo
+ */
+public class Ahorcado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
